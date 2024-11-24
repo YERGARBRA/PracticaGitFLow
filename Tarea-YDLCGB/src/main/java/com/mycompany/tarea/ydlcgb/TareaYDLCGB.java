b8381099cb67bc7c6a5e1e8404d99e269cbddd2b
YERGARBRA <yergarbra@alu.edu.gva.es>
2024-11-24 20:27:00 +0100

    Creación del proyecto en NetBeans

---
+++ Tarea-YDLCGB/src/main/java/com/mycompany/tarea/ydlcgb/TareaYDLCGB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea.ydlcgb;
+
+/**
+ *
+ * @author Yeray
+ */
+public class TareaYDLCGB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
