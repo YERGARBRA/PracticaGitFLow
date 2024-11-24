77f737976a759e355dead9dea2f756fe57ad0ea8
YERGARBRA <yergarbra@alu.edu.gva.es>
2024-11-24 21:22:26 +0100

    Implementada clase Cuadrado.java

--- Tarea-YDLCGB/src/main/java/com/mycompany/tarea/ydlcgb/Cuadrado.java
+++ Tarea-YDLCGB/src/main/java/com/mycompany/tarea/ydlcgb/Cuadrado.java
@@ -5,9 +5,45 @@
 package com.mycompany.tarea.ydlcgb;
 
 /**
- *
+ * Implementación de una figura geométrica tipo Cuadrado
  * @author Yeray
  */
-public class Cuadrado {
-    
+public class Cuadrado implements Figura {
+
+    private int lado;
+
+    /**
+     * *
+     * Constructor
+     *
+     * @param lado
+     */
+    public Cuadrado(int lado) {
+        this.lado = lado;
+    }
+
+    /* Getters and Setters */
+    public int getLado() {
+        return lado;
+    }
+
+    public void setLado(int lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public String toString() {
+        return "Cuadrado{" + "lado=" + lado + '}';
+    }
+
+    /* Implementación de la interfaz */
+    @Override
+    public double calcularArea() {
+        return lado * lado;
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 4 * lado;
+    }
 }
